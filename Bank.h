e884a51befe80c2acf39d6a05fa20e2ba544eb59
mseeg <Canismajoris@gmx.com>
2013-10-02 23:27:20 -0500

    Update Bank.h

--- Bank.h
+++ Bank.h
@@ -2,16 +2,20 @@
 
 class Bank
 {
-    int anum;
-    int a_num;
 public:
+    int vplace; //Vector placement
+    int a_num; //Account number
+    int low;
+
     Account* all;
     
-    int callanum()  {return anum;};
+    /*int callvplace()  {return vplace;};
     int calla_num() {return a_num;};
+    int calllow() {return low;};
     
-    int addanum()   {return ++anum;};
-    int add_anum()  {return ++a_num;};
+    int addvplace()   {return ++vplace;};
+    int add_anum()  {return ++a_num;};*/
+    
     
     Bank()
     : all(new Account[100]),anum(1),a_num(600001){}
