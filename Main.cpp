df5c2d6e9ade4e9f2af19f44ef22817d4f6a331d
mseeg <Canismajoris@gmx.com>
2013-10-02 23:20:35 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,11 +10,163 @@ void fill(Bank& All) //Fill the bank with default accounts
     }
 }
 
+void in(Bank& All) //Write in what is to be stored in Bank
+{
+    int spot=2;
+    int linecount=1;
+    int reset=0;
+    
+    string fname;
+    string lname;
+    string s_a_number;
+    string s_pin;
+    string s_balance;
+    
+    int i_a_number=0;
+    int i_pin=0;
+    int i_balance=0;
+    
+    ifstream ifs1("Bank.txt", ifstream::in);
+    
+    while(true)
+    {
+        string line;
+        
+        if(linecount==1) fname="";
+        if(linecount==1) lname="";
+        if(linecount==2) s_a_number="";
+        if(linecount==3) s_pin="";
+        if(linecount==4) s_balance="";
+        
+        if(linecount==1)
+        {
+            i_a_number=0;
+            i_pin=0;
+            i_balance=0;
+        }
+        
+        getline(ifs1,line);
+        
+        if(linecount==6)
+        {
+            reset=1;
+        }
+        
+        if(linecount==5)
+        {
+            spot=18;
+            for(int i=1;i<11;++i)
+            {
+                string T="";
+                for (spot;spot<line.size(); ++spot)
+                {
+                    if(line[spot]!=')')
+                    {
+                        T += line[spot];
+                    }
+                    else
+                    {
+                        spot=spot+3;
+                        break;
+                    }
+                }
+                *called=All.all[(i_a_number-600000)];
+                called->transactions[i]=T;
+                if(i==10) ++linecount;
+            }
+        }
+        
+        if(linecount==4)
+        {
+            for (spot=10;spot<line.size(); ++spot)
+            {
+                s_balance += line[spot];
+                if ((spot+1)==(line.size()))
+                {
+                    istringstream (s_balance) >> i_balance; //Convert string to int
+                    Account* A= new Account(fname,lname,i_a_number,i_pin,i_balance);
+                    All.all[All.vplace]=*A;
+                    ++linecount;
+                    ++linecount;
+                }
+            }
+        }
+        
+        if(linecount==3)
+        {
+            for (spot=6;spot<line.size(); ++spot)
+            {
+                s_pin += line[spot];
+                if ((spot+1)==(line.size()))
+                {
+                    istringstream (s_pin) >> i_pin; //Convert string to int
+                    ++linecount;
+                }
+            }
+            
+        }
+        
+        if(linecount==2) //Account number
+        {
+            for(spot=10;spot<line.size(); ++spot)
+            {
+                s_a_number += line[spot];
+                
+                if ((spot+1)==(line.size()))
+                {
+                    istringstream (s_a_number) >> i_a_number; //Convert string to int
+                    if((i_a_number-600000)>All.vplace) All.vplace=(i_a_number-600000);
+                    if((i_a_number-600000)<All.low && i_a_number!=0) All.low=(i_a_number-600000);
+                    All.a_num=i_a_number;
+                    ++linecount;
+                }
+            }
+        }
+        
+        if(linecount==1) //First and last name
+        {
+            for (spot=11;spot<line.size(); ++spot) //n starts at 11 because the beginning of the first name is 11 characters over.
+            {
+                if (line[spot]!=' ')
+                {
+                    fname += line[spot];
+                }
+                else
+                {
+                    ++spot;
+                    break;
+                }
+            }
+            
+            for (;spot<line.size(); ++spot)
+            {
+                lname += line[spot];
+                if ((spot+1)==(line.size()))
+                {
+                    ++linecount;
+                }
+            }
+            
+        }
+        
+        if(reset==1)
+        {
+            linecount=1;
+            reset=0;
+        }
+        
+        if(ifs1.eof()) break;
+        ifs1.clear();
+    }
+    ++All.vplace;
+    ++All.a_num;
+}
+
 void out(Bank& All) //Write out what is stored in Bank
 {
     ofstream ofs1("Bank.txt", ofstream::out); 
     
-    for(int i=1;i<All.callvplace();++i)
+    for(int i=All.low;i<All.vplace;++i)
     {
         *called=All.all[i];
         if(called->balance!=0)
@@ -46,13 +198,13 @@ void create(Bank& All)
     
     pin=rand() % 9000 + 1000;
     
-    Account* A= new Account(fname,lname,All.calla_num(),pin,1000);
+    Account* A= new Account(fname,lname,All.a_num,pin,1000);
     
-    cout << "Your account number is " << All.calla_num() << " and your pin number is " << pin << ".\n";
+    cout << "Your account number is " << All.a_num << " and your pin number is " << pin << ".\n";
     cout << "Your beginning balance is $1000.\n";
     
-    All.all[All.callvplace()]=*A;
-    All.addvplace() && All.add_anum();
+    All.all[All.vplace]=*A;
+    ++All.vplace && ++All.a_num;
 }
 
 
@@ -61,6 +213,7 @@ int main()
     Bank All;
     fill(All); 
     
+    in(All);
     cout << "This is your Piggy Bank. Welcome valued customer.\n";
     int operation=0;
     cout    << "Please select one of these operations (enter a number): \n"
