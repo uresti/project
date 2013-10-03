8c11d341e9e78f600894fdb1849364dc021d12ee
mseeg <Canismajoris@gmx.com>
2013-10-02 22:02:53 -0500

    Update Bank.h

--- Bank.h
+++ Bank.h
@@ -2,10 +2,17 @@
 
 class Bank
 {
+    int anum;
+    int a_num;
 public:
-    Account* all=new Account[100];
-    Account* Accounts= new Account[100];
+    Account* all;
+    
+    int callanum()  {return anum;};
+    int calla_num() {return a_num;};
+    
+    int addanum()   {return ++anum;};
+    int add_anum()  {return ++a_num;};
     
     Bank()
-    : all(Accounts) {}
+    : all(new Account[100]),anum(1),a_num(600001){}
 };
