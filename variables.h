e39333b0c0e95e9118c977facc712ab25c6f339a
uresti <uresti77@neo.tamu.edu>
2013-10-07 17:22:19 -0500

    Create variables.h

---
+++ variables.h
@@ -0,0 +1,16 @@
+#include "std_lib_facilities_3.h" 
+
+int number_of_accounts = 0; 
+bool finalize = true; 
+
+string names_file = "names.txt";
+string accounts_file = "accounts.txt";
+string pins_file = "pins.txt"; 
+
+vector<int>all_accounts_num; 
+vector<int>all_pins; 
+vector<string>account_names;
+vector<string>customer_files; 
+
+vector<string>bank_info; 
+string bank_file = "bank.txt"; 
