65f9cd238500a89f12abfbd61c5b556d77ba8860
mseeg <Canismajoris@gmx.com>
2013-10-09 17:26:04 -0500

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cstdlib>
 #include <fstream>
 #include <sstream>
+
+
+using namespace std;
 
 class Customer
 {
 public:
 	string first_name;
 	string last_name;
-	string* transactions;
+    	string* transactions;
 	int account_number;
 	int pin_number;
 	int balance;
 	
 	Customer(const string& f,const string& l ,const int a, const int p, int bal)
-    		: first_name(f), last_name(l), account_number(a), pin_number(p), balance(bal), transactions(new string [11]) {}
+		 : first_name(f), last_name(l), account_number(a), pin_number(p), balance(bal), transactions(new string [11]) {}
+
+    	Customer() {};
 };
