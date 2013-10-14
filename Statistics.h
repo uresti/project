9af37ab5a8b90af453b675356b1afc919172629f
uresti <uresti77@neo.tamu.edu>
2013-10-14 15:04:22 -0500

    Update Statistics.h

--- Statistics.h
+++ Statistics.h
@@ -1,12 +1,22 @@
-#include "Bank.h"
+#include "Statistics.h"
 
-
-class Statistics
+class Bank
 {
-	public:
-	void total_customers() {};
-	void total_money(){};
-	void average_money(){};	
-	void history();
-	void info();
+public:
+    int vplace; //Vector placement
+    int a_num; //Account number ~Has been removed.
+    int low; //~Has been removed.
+
+    Customer* all;
+    
+    Bank()
+    : all(new Customer[100]),vplace(0),a_num(600001),low(100){}
+    
+    void fill();
+    void create(Statistics& Info);
+    void close(Statistics& Info);
+    void withdraw(int a_number,Statistics& Info);
+    void deposit(int a_number,Statistics& Info);
+    void account_balance(int a_number);
+    void transactions(int a_number);
 };
